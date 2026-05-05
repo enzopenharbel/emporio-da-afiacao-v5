33d803adfc7611458e914bea515c1bdbda537dfc
enzopenharbel <enzopenharbel33@gmail.com>
2026-05-05 17:07:09 -0300

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,13 @@
+export const products = [
+  {
+    id: "alicate-mundial-522",
+    nome: "Alicate Mundial 522",
+    categoria: "Alicates",
+    descricao: "Alicate para cutículas Mundial 522.",
+    imagens: [
+      "/images/produtos/alicate-mundial-522.jpg",
+      "/images/produtos/alicate-mundial-522-2.jpg",
+      "/images/produtos/alicate-mundial-522-3.jpg"
+    ]
+  }
+];
